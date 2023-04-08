46ad735ac88f63a9bf0ce94ad375e3646688ce5d
aakashdeveloper <ahanda205@gmail.com>
2023-04-08 20:39:35 +0530

    datatype

---
+++ javascript/1datatypes.js
@@ -0,0 +1,112 @@
+es5 > Ecma Script 5
+es6 > Ecma Script 6
+
+Datatypes
+>> String > "Hii" 'hey' `hello` "10"  'true'
+>> Number > 1000 4546557575 .4  3543.445  3
+>> Boolean > true false
+
+
+//es5
+var a = "Hiii"
+var b = 10
+var c = true
+
+typeof(a)
+'string'
+typeof(b)
+'number'
+typeof(c)
+'boolean'
+
+var d = "10"
+typeof(d)
+'string'
+
+var e = 10.34
+typeof(e)
+'number'
+
+a+b
+30
+a-b
+-10
+b-a
+10
+a*b
+200
+a/b
+0.5
+
+4%2
+    
+0
+5%2
+1
+5%3
+2
+
+
+2%3
+2
+2%4
+2
+
+
+var a = "Hiii"
+var b = "JavaScript"
+
+var a = "Hiii"
+var b = "JavaScript"
+a+b
+'HiiiJavaScript'
+a-b
+NaN (Not a number)
+a*b
+NaN
+a/b
+NaN
+"hii"+10
+'hii10'
+10+"hii"
+'10hii'
+
+
+string + string = string 
+string + number = string 
+number + string = string 
+number + number = number 
+
+"10"+20+30
+"1020"+30
+'102030'
+
+10+"20"+30
+"1020"+30
+'102030'
+
+10+20+"30"
+30+"30"
+'3030'
+
+"10"+20+30-1
+102029
+10+"20"+30-1
+102029
+10+20+"30"-1
+3029
+
+
+"10"-1
+9
+20-"50"
+-30
+20+"50"
+'2050'
+
+"10"*"20"
+200
+"10"/"20"
+0.5
+"10a"/"20"
+NaN
---
+++ javascript/2datatype.js
@@ -0,0 +1,77 @@
+true
+false 
+
+true is equal to 1
+false is equal to 0
+
+true + true
+2
+true + false
+1
+
+10+true
+11
+10/false
+Infinity
+"10"+true
+'10true'
+"10"-true
+9
+
+var a = "10"
+var b = "20"
+undefined
+a+b
+'1020'
+parseInt(a)
+10
+var c = "a"
+undefined
+parseInt(c)
+NaN
+parseInt(a)+parseInt(b)
+30
+
+var a = "10.12"
+var b = "20.11"
+undefined
+parseInt(a)+parseInt(b)
+30
+parseInt(a+b)
+10
+parseFloat(a)+parseFloat(b)
+30.229999999999997
+
+
+var a = "10Ab"
+var b = "Ab10"
+
+parseInt(a)
+10
+parseInt(b)
+NaN
+
+
+//es6
+var a = "10"
+var b = "20"
+undefined
+Number(a)+Number(b)
+30
+var a = "10"
+var b = "20"
+undefined
+Number(a)+Number(b)
+30
+var a = "10.12"
+var b = "20.11"
+undefined
+Number(a)+Number(b)
+30.229999999999997
+var a = "10Ab"
+var b = "Ab10"
+undefined
+Number(a)+Number(b)
+NaN
+Number(a)
+NaN
