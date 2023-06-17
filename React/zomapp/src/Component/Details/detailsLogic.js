fbcc8a8a7a7d6abda5fd7f786670a4a2cc0d5301
Bhumika Bajpai <bhumikabajpai@Bhumikas-MacBook-Air.local>
2023-06-17 20:20:10 +0530

    details

---
+++ React/zomapp/src/Component/Details/detailsLogic.js
@@ -0,0 +1,66 @@
+import React,{useState,useEffect} from 'react';
+import './details.css';
+import axios from 'axios';
+import {Link} from 'react-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
+
+const base_url = "http://3.17.216.66:4000";
+
+const DetailDisplay = () => {
+
+    let [searchParams] = useSearchParams()
+    let [restDetails, setrestDetails] = useState()
+
+    let restId = searchParams.getAll('restId');
+    const restDetail = async() => {
+        const rdata = await axios.get(`${base_url}/details/${restId}`);
+        setrestDetails(rdata.data[0])
+    }
+
+    useEffect(() => {
+       restDetail() 
+    },[])
+
+    const renderDetails = () => {
+        if(restDetails){
+            return(
+                <>
+                    <div className='tileImage'>
+                    <div className='imageClass'>
+                        <img src={restDetails.restaurant_thumb}
+                        alt={restDetails.restaurant_name}/>
+                    </div>
+                    </div>
+                    <div className='tileContent'>
+                        <div className='content'>
+                            <h1>{restDetails.restaurant_name}</h1>
+                            <span id="cfeedback">231 Customers Rating Average</span>
+                            <h3>Old Price <del>Rs. 450</del></h3>
+                            <h3>Offer Price Rs. {restDetails.cost}</h3>
+                            <h3>Best Taste of Fresh Chai with Samosa At your Door or DineIn</h3>
+                                <div>
+                                    <div className="icons">
+                                        <img src="https://i.ibb.co/wJvrhYg/veg.png" alt=""/>
+                                    </div>
+                                    <div className="icons">
+                                        <img src="https://i.ibb.co/mD3jpgc/sentizied.png" alt=""/>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                </>
+            )
+        }
+    }
+
+
+    return(
+        <>
+            <div className='main'>
+                {renderDetails()}
+            </div>
+        </>
+    )
+}
+
+export default DetailDisplay
--- React/zomapp/src/Component/Header.js
+++ React/zomapp/src/Component/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.css';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
     return(
         <>
             <header>
                 <div id="brand">
-                    Developer Funnel
+                    Developer Funnel &nbsp;&nbsp; 
+                    <Link className='btn btn-info' to="/">Home</Link>
                 </div>
                 <div id="social">
                     <a href="">
